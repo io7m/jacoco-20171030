eb123ee661d984b47fc857a96b0995cf6058a5d0
Mark Raynsford <code@io7m.com>
2017-10-30 17:12:09 +0000

    Initial version

---
+++ core/src/main/java/com/io7m/example/core/Core.java
@@ -0,0 +1,15 @@
+package com.io7m.example.core;
+
+public final class Core
+{
+  private Core()
+  {
+
+  }
+
+  public static void main(
+    final String[] args)
+  {
+    System.out.println("Hello");
+  }
+}
---
+++ tests/src/test/java/com/io7m/example/tests/CoreTest.java
@@ -0,0 +1,13 @@
+package com.io7m.example.tests;
+
+import com.io7m.example.core.Core;
+import org.junit.jupiter.api.Test;
+
+public class CoreTest
+{
+  @Test
+  public void testHello()
+  {
+    Core.main(new String[] { });
+  }
+}
